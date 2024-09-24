1678599a6967d28c8723c5d2e9095378b8411a67
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-24 20:27:48 -0300

    Create Project

---
+++ src/test/java/com/mymoney/finance_control/FinanceControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymoney.finance_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
