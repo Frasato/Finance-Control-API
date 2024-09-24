93ea3ccef2560ef2ad828dbc91e1eebfa7cfeb07
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-24 20:28:58 -0300

    Create User Service

---
+++ src/main/java/com/mymoney/finance_control/UserService.java
@@ -0,0 +1,13 @@
+package com.mymoney.finance_control;
+
+import com.mymoney.finance_control.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+}
