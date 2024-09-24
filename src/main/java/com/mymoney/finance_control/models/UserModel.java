8d381ef0b21cca2f5ee37e351370e42c718ba03a
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-24 20:28:53 -0300

    Create User Model

---
+++ src/main/java/com/mymoney/finance_control/models/UserModel.java
@@ -0,0 +1,35 @@
+package com.mymoney.finance_control.models;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "user_tb")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<MoneyModel> moneyModelList;
+
+}
