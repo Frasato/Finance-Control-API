aedc0f6ca5d66865ad458e97a5e9268c5a4d5108
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-25 12:39:15 -0300

    Create function to add money in account

--- src/main/java/com/mymoney/finance_control/controllers/UserController.java
+++ src/main/java/com/mymoney/finance_control/controllers/UserController.java
@@ -1,14 +1,17 @@
 package com.mymoney.finance_control.controllers;
 
+import com.mymoney.finance_control.dtos.MoneyDto;
 import com.mymoney.finance_control.dtos.UserDto;
+import com.mymoney.finance_control.models.MoneyModel;
 import com.mymoney.finance_control.models.UserModel;
+import com.mymoney.finance_control.services.MoneyService;
 import com.mymoney.finance_control.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/auth")
@@ -16,6 +19,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private MoneyService moneyService;
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody UserDto userDto){
@@ -43,6 +49,28 @@ public class UserController {
             return ResponseEntity.status(200).body(user);
         }else{
             return ResponseEntity.status(401).body("Invalid Credentials");
+        }
+    }
+
+    @PostMapping("/add/{userId}")
+    public ResponseEntity<?> addMoneyOnAccount(@PathVariable("userId")UUID userId, @RequestBody MoneyDto moneyDto){
+        Optional<UserModel> findedUser = userService.findUserById(userId);
+
+        MoneyModel money = new MoneyModel();
+
+        if(findedUser.isPresent()){
+            UserModel user = findedUser.get();
+
+            money.setMoneyOut(0.0);
+            money.setMoneyIn(moneyDto.getMoneyIn());
+            money.setForWhat(moneyDto.getForWhat());
+            money.setCategory(moneyDto.getCategory());
+            money.setUserId(user);
+
+            moneyService.saveMoneyIn(money);
+            return ResponseEntity.status(201).body(money);
+        }else{
+            return ResponseEntity.status(404).body("User not found to save a money!");
         }
     }
 
