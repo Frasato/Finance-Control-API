e6d08cb2dc76e3418d7ee2f7480f297cbf1ad570
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-25 09:24:21 -0300

    Create user service and functions to register and login

---
+++ src/main/java/com/mymoney/finance_control/services/UserService.java
@@ -0,0 +1,45 @@
+package com.mymoney.finance_control.services;
+
+import com.mymoney.finance_control.models.UserModel;
+import com.mymoney.finance_control.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void registerUser(UserModel userModel){
+
+        if(userModel.getUsername().isEmpty() || userModel.getPassword().isEmpty()){
+            throw new RuntimeException("Empty fields!");
+        }
+
+        userModel.setPassword(passwordEncoder.encode(userModel.getPassword()));
+        userModel.setUsername(userModel.getUsername().toLowerCase().trim());
+        userRepository.save(userModel);
+    }
+
+    public boolean validateUser(UserModel userModel){
+        Optional<UserModel> findedUser = userRepository
+                .findUserByUsername(userModel
+                        .getUsername()
+                        .toLowerCase()
+                        .trim()
+                );
+
+        if(findedUser.isPresent()){
+            return passwordEncoder.matches(userModel.getPassword(), findedUser.get().getPassword());
+        }else{
+            return false;
+        }
+    }
+}
