d8e926fcb7241ae6d639fbfe3d57ab981cccf28c
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-25 12:38:44 -0300

    Remove lombok and add getters and setters

--- src/main/java/com/mymoney/finance_control/dtos/UserDto.java
+++ src/main/java/com/mymoney/finance_control/dtos/UserDto.java
@@ -1,13 +1,23 @@
 package com.mymoney.finance_control.dtos;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class UserDto {
 
     private String username;
     private String password;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
