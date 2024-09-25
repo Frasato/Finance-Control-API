93cbf30dedd2963e682ed301d3c3a491c3372cb9
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-25 09:22:59 -0300

    Create User DTO

---
+++ src/main/java/com/mymoney/finance_control/dtos/UserDto.java
@@ -0,0 +1,13 @@
+package com.mymoney.finance_control.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDto {
+
+    private String username;
+    private String password;
+
+}
