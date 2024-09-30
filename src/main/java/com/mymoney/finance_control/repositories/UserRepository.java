0e5064da914a13e1a2f40e1d4934fa825e4e08fb
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-30 14:45:03 -0300

    Add findByEmail function

--- src/main/java/com/mymoney/finance_control/repositories/UserRepository.java
+++ src/main/java/com/mymoney/finance_control/repositories/UserRepository.java
@@ -4,5 +4,9 @@ import com.mymoney.finance_control.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {}
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+}
