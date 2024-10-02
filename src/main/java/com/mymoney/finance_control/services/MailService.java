b569cdbdbeddbfe2e0352d612ec95c2de9dffb7f
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-02 09:50:20 -0300

    Add UTF-8 codification for email

--- src/main/java/com/mymoney/finance_control/services/MailService.java
+++ src/main/java/com/mymoney/finance_control/services/MailService.java
@@ -5,9 +5,7 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
@@ -318,7 +316,7 @@ public class MailService {
                 """;
 
         MimeMessage message = javaMailSender.createMimeMessage();
-        MimeMessageHelper messageHelper = new MimeMessageHelper(message);
+        MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
 
         messageHelper.setFrom(fromAddress, senderName);
         messageHelper.setTo(toAddress);
