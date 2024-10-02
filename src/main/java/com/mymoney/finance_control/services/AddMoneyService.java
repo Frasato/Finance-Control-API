a9cc898fd9d0d9402b4a1067dd66d7e4514403ae
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-02 10:39:14 -0300

    Create listAll and Save methods on add money service

--- src/main/java/com/mymoney/finance_control/services/AddMoneyService.java
+++ src/main/java/com/mymoney/finance_control/services/AddMoneyService.java
@@ -1,9 +1,24 @@
 package com.mymoney.finance_control.services;
 
+import com.mymoney.finance_control.models.AddMoney;
+import com.mymoney.finance_control.repositories.AddMoneyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AddMoneyService {
+
+    @Autowired
+    private AddMoneyRepository addMoneyRepository;
+
+    public List<AddMoney> listAll(){
+        return addMoneyRepository.findAll();
+    }
+
+    public void save(AddMoney addMoney){
+        addMoneyRepository.save(addMoney);
+    }
 
 }
