67dc52b41ca29956b78b79ff1bfccb202d61f535
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-02 12:59:15 -0300

    Using a query to find all removed money by user id

--- src/main/java/com/mymoney/finance_control/repositories/RemoveMoneyRepository.java
+++ src/main/java/com/mymoney/finance_control/repositories/RemoveMoneyRepository.java
@@ -1,8 +1,17 @@
 package com.mymoney.finance_control.repositories;
 
+import com.mymoney.finance_control.models.AddMoney;
 import com.mymoney.finance_control.models.RemoveMoney;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface RemoveMoneyRepository extends JpaRepository<RemoveMoney, String> {}
+public interface RemoveMoneyRepository extends JpaRepository<RemoveMoney, String> {
+
+    @Query(value = "SELECT * FROM remove_money WHERE user_id=%:id%", nativeQuery = true)
+    List<RemoveMoney> findByUserId(@Param("id") String id);
+}
