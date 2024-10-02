eda759a8e75a09debe31f1ea9a078b01257e600a
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-02 10:06:14 -0300

    Create Relationship on tables

--- src/main/java/com/mymoney/finance_control/models/User.java
+++ src/main/java/com/mymoney/finance_control/models/User.java
@@ -1,6 +1,9 @@
 package com.mymoney.finance_control.models;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -13,14 +16,24 @@ public class User {
     private String email;
     private String password;
 
+    @OneToMany(mappedBy = "userAdd", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<AddMoney> addMoney;
+
+    @OneToMany(mappedBy = "userRemove", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<RemoveMoney> removeMoney;
+
     public User() {
     }
 
-    public User(String id, String username, String email, String password) {
+    public User(String id, String username, String email, String password, List<AddMoney> addMoney, List<RemoveMoney> removeMoney) {
         this.id = id;
         this.username = username;
         this.email = email;
         this.password = password;
+        this.addMoney = addMoney;
+        this.removeMoney = removeMoney;
     }
 
     public String getId() {
@@ -53,5 +66,21 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<AddMoney> getAddMoney() {
+        return addMoney;
+    }
+
+    public void setAddMoney(List<AddMoney> addMoney) {
+        this.addMoney = addMoney;
+    }
+
+    public List<RemoveMoney> getRemoveMoney() {
+        return removeMoney;
+    }
+
+    public void setRemoveMoney(List<RemoveMoney> removeMoney) {
+        this.removeMoney = removeMoney;
     }
 }
