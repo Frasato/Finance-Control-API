f8fe0bb77c39a0f1590172b44dfa17bb2fbbba5a
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-02 11:36:32 -0300

    Create a select query to find by user id

--- src/main/java/com/mymoney/finance_control/repositories/AddMoneyRepository.java
+++ src/main/java/com/mymoney/finance_control/repositories/AddMoneyRepository.java
@@ -2,7 +2,15 @@ package com.mymoney.finance_control.repositories;
 
 import com.mymoney.finance_control.models.AddMoney;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface AddMoneyRepository extends JpaRepository<AddMoney, String> {}
+public interface AddMoneyRepository extends JpaRepository<AddMoney, String> {
+
+    @Query(value = "SELECT * FROM add_money WHERE user_id=%:id%", nativeQuery = true)
+    List<AddMoney> findByUserId(@Param("id") String id);
+}
