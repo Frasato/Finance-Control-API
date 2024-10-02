c45ffc93935c7ea460114cf4442c433b0ed15bdf
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-02 12:23:33 -0300

    Create List All method

--- src/main/java/com/mymoney/finance_control/services/RemoveMoneyService.java
+++ src/main/java/com/mymoney/finance_control/services/RemoveMoneyService.java
@@ -1,13 +1,20 @@
 package com.mymoney.finance_control.services;
 
+import com.mymoney.finance_control.models.RemoveMoney;
 import com.mymoney.finance_control.repositories.RemoveMoneyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RemoveMoneyService {
 
     @Autowired
     private RemoveMoneyRepository removeMoneyRepository;
+
+    public List<RemoveMoney> listAllRemovedMoney(){
+        return removeMoneyRepository.findAll();
+    }
 
 }
