70799d46be74ac48ef3ab12cc7d38d37b27fd2b4
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-03 16:19:14 -0300

    Create a sum for a negative money

--- src/main/java/com/mymoney/finance_control/controller/SumController.java
+++ src/main/java/com/mymoney/finance_control/controller/SumController.java
@@ -1,7 +1,9 @@
 package com.mymoney.finance_control.controller;
 
 import com.mymoney.finance_control.models.AddMoney;
+import com.mymoney.finance_control.models.RemoveMoney;
 import com.mymoney.finance_control.services.AddMoneyService;
+import com.mymoney.finance_control.services.RemoveMoneyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,8 @@ public class SumController {
 
     @Autowired
     private AddMoneyService addMoneyService;
+    @Autowired
+    private RemoveMoneyService removeMoneyService;
 
     @GetMapping
     public ResponseEntity<?> sumPositiveMoney(){
@@ -25,6 +29,22 @@ public class SumController {
         if(!allAddMoney.isEmpty()){
             for (AddMoney addMoney : allAddMoney) {
                 totalValue = totalValue + addMoney.getValue();
+            }
+
+            return ResponseEntity.status(200).body(totalValue);
+        }else{
+            return ResponseEntity.status(200).body(0);
+        }
+    }
+
+    @GetMapping("/outs")
+    public ResponseEntity<?> sumNegativeMoney(){
+        List<RemoveMoney> allRemovedMoney = removeMoneyService.listAllRemovedMoney();
+        double totalValue = 0;
+
+        if(!allRemovedMoney.isEmpty()){
+            for(int i = 0; i < allRemovedMoney.size(); i++){
+                totalValue = totalValue + allRemovedMoney.get(i).getValue();
             }
 
             return ResponseEntity.status(200).body(totalValue);
