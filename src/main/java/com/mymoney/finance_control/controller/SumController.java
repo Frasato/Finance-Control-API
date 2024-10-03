609f8429bdcbdb9c463b4878fcf2a856b51038ef
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-03 16:18:47 -0300

    Create a sum for a positive money

--- src/main/java/com/mymoney/finance_control/controller/SumController.java
+++ src/main/java/com/mymoney/finance_control/controller/SumController.java
@@ -1,12 +1,36 @@
 package com.mymoney.finance_control.controller;
 
+import com.mymoney.finance_control.models.AddMoney;
+import com.mymoney.finance_control.services.AddMoneyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/sum")
 public class SumController {
 
+    @Autowired
+    private AddMoneyService addMoneyService;
 
+    @GetMapping
+    public ResponseEntity<?> sumPositiveMoney(){
+        List<AddMoney> allAddMoney = addMoneyService.listAll();
+        double totalValue = 0;
+
+        if(!allAddMoney.isEmpty()){
+            for (AddMoney addMoney : allAddMoney) {
+                totalValue = totalValue + addMoney.getValue();
+            }
+
+            return ResponseEntity.status(200).body(totalValue);
+        }else{
+            return ResponseEntity.status(200).body(0);
+        }
+    }
 
 }
