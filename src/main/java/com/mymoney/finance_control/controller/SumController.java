d5fd6ef9e42d32bfec80d562c9d482e0be1bab8a
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-03 16:17:38 -0300

    Create sum end point

---
+++ src/main/java/com/mymoney/finance_control/controller/SumController.java
@@ -0,0 +1,12 @@
+package com.mymoney.finance_control.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sum")
+public class SumController {
+
+
+
+}
